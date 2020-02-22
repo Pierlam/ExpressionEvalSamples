e1afe1fa1569ade5d809bf76d55318103b3cc3fb
pierre <pierlam-project@outlook.com>
2020-02-22 17:51:42 +0100

    Ajoutez des fichiers projet.

---
+++ TestExpressionEvalNetCoreApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Pierlam.ExpressionEval;
+using System;
+
+namespace TestExpressionEvalNetCoreApp
+{
+    /// <summary>
+    /// Test the library ExpressionEval
+    /// .NET Core.
+    /// Use the netstandard2.0 version.
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionEval expressionEval = new ExpressionEval();
+            
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
